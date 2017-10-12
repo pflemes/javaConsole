b0a319adbee7a95179b63b8d79fb146d7d1784db
pflemes <pedro_lemes@msn.com>
2017-10-12 08:19:51 -0300

    Alteração Main

--- src/br/com/javaConsole/javaConsole.java
+++ src/br/com/javaConsole/javaConsole.java
@@ -20,6 +20,7 @@ public class javaConsole {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("GitHub");
         sqlCls sql = new sqlCls();
         //sql.insert();
         sql.selectAll();
